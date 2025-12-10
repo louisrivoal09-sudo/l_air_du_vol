d5fc457c27bdc8237e7cd1e0343705a10a3f039f
louisrivoal09-sudo <louis.rivoal09@gmail.com>
2025-12-10 11:57:09 +0100

    2ème commit

---
+++ dblouis/donnelouis/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DonnelouisConfig(AppConfig):
+    name = 'donnelouis'
---
+++ dblouis/donnelouis/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Article(models.Model):
+    titre = models.CharField(max_length=200)
+    contenu = models.TextField()
+    auteur = models.CharField(max_length=100)
+    date_publication = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titre
