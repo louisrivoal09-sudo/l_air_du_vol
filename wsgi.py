2a367b1cba8cfbe58c5e449735e2bf90500fa599
louisrivoal09-sudo <louis.rivoal09@gmail.com>
2026-01-03 14:48:23 +0100

    Fix wsgi.py - add sys.path before django import

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,13 @@
 import os
 import sys
-import django
 
-# Ajouter le chemin vers louis/dblouis
+# Ajouter le chemin vers louis/dblouis AVANT d'importer Django
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.join(BASE_DIR, 'louis', 'dblouis'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dblouis.settings')
 
+import django
 django.setup()
 
 from django.core.wsgi import get_wsgi_application
